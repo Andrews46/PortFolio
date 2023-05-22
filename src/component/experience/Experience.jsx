5104868f8aa33c22d23ce48851e844db8944158a
Andrews46 <andrea4691ct@gmail.com>
2023-05-22 20:18:49 +0200

    esercizio

--- src/component/experience/Experience.jsx
+++ src/component/experience/Experience.jsx
@@ -8,7 +8,7 @@ const Experience = () => {
     <div onClick={onHadleClick} id="experience" className={styles.Experience}>
       <div className={styles.container}>
         <h2>Experience</h2>
-        <h3>FRONT-END DEVELOPER</h3>
+        <h3 className={styles.titleExperience}>FRONT-END DEVELOPER</h3>
         <p>
           Edgemony Code-week: sviluppo applicazione meteo zona di Palermo e
           provincia. Realizzata con: HTML, CSS, JAVASCRIPT Vanilla. Final
@@ -17,7 +17,7 @@ const Experience = () => {
           CSS modules, React router e Firebase.
         </p>
         <hr />
-        <h3>LAVORO AUTONOMO</h3>
+        <h3 className={styles.titleExperience}>LAVORO AUTONOMO</h3>
         <p>
           2019 - Presente: Creazione di prodotti in legno handmade di
           riproduzione di armi di arti marziali e armi per cosplayer, oggetti
@@ -25,13 +25,13 @@ const Experience = () => {
           allo store
         </p>
         <hr />
-        <h3>MAESTRO DI ARTI MARZIALI</h3>
+        <h3 className={styles.titleExperience}>MAESTRO DI ARTI MARZIALI</h3>
         <p>
           Febbraio 2018 - Presente: Maestro di Wing Chun e difesa personale,
           membro ASI, associazione sportiva.
         </p>
         <hr />
-        <h3>EDILIZIA</h3>
+        <h3 className={styles.titleExperience}>EDILIZIA</h3>
         <p>
           2012 - 2018 : Sistema di costruzione a secco: pareti e controsoffitti
           interni ed esterni, cartongesso tecnico d'arredo
